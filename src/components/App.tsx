9551d3e22a220c682e19eb1bcdc57965a5f04d5b
YagoLopez <yago.lopez@gmail.com>
2018-01-19 21:06:48 +0100

    Refactors

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,17 +1,14 @@
-import * as React from "react";
+import * as React from 'react';
 
 
 export default class App extends React.Component<{}, {}> {
 
-
   public render(): JSX.Element {
-
     return (
-      <div style={{flex: 1, textAlign: 'center'}}>
+      <main className="container">
         <h1>Starter Project</h1>
         <div>React + TypeScript + Parcel</div>
-      </div>
+      </main>
     );
   }
-
 }
